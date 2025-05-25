0b8eaf01844f56df8fc487dbc7c5b15344dbd494
Thomas <huytgce170732@fpt.edu.vn>
2025-05-25 19:10:10 +0700

    OPTV-2 feat: add register feature

--- src/modules/otp-verifications/otp-verifications.repository.ts
+++ src/modules/otp-verifications/otp-verifications.repository.ts
@@ -4,5 +4,28 @@ import { OtpType, OtpVerification } from './otp-verifications.schema';
 import { Model } from 'mongoose';
 @Injectable()
 export class OtpVerificationRepository {
-  
+  constructor(@InjectModel(OtpVerification.name) private otpVerification: Model<OtpVerification>) {}
+  async saveOTP(
+    email: string,
+    otp: string,
+    count: number,
+    type: OtpType,
+  ): Promise<OtpVerification> {
+    return await this.otpVerification.findOneAndUpdate(
+      { email, otpType: type },
+      {
+        otp: otp,
+        count: count,
+        expiresAt: new Date(),
+      },
+      { upsert: true, new: true },
+    );
+  }
+
+  async findOtpVerificationByEmailAndOtpType(
+    email: string,
+    otp: OtpType,
+  ): Promise<OtpVerification | null> {
+    return await this.otpVerification.findOne({ email, otpType: otp });
+  }
 }
