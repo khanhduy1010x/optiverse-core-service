a9ad2821cc536cde9cfa968956a97e47561bb1e8
Tan Loi <LoiTTCE171879@fpt.edu.vn>
2025-06-05 20:58:31 +0700

    OPTV-7 : fix update avatar

--- src/modules/profiles/profile.module.ts
+++ src/modules/profiles/profile.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProfileController } from './profile.controller';
 import { UsersModule } from '../users/user.module';
 import { UserSessionModule } from '../users-sessions/user-session.module';
+import { CloudinaryModule } from '../../common/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [UsersModule, UserSessionModule],
+  imports: [UsersModule, UserSessionModule, CloudinaryModule],
   controllers: [ProfileController],
 })
 export class ProfilesModule {}
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -2,20 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { User } from './user.schema';
 import { UserRepository } from './user.repository';
 import { UpdateProfileRequest } from '../profiles/dto/request/UpdateProfileRequest.dto';
+
 @Injectable()
 export class UserService {
   constructor(private userRepository: UserRepository) {}
+
   async findOne(username: string): Promise<User | null> {
     return await this.userRepository.findOne(username);
   }
-   async findOneByEmail(email: string): Promise<User | null> {
+
+  async findOneByEmail(email: string): Promise<User | null> {
     return await this.userRepository.findByEmail(email);
   }
+
   async updateVerifyAccount(email: string): Promise<User | null> {
     return await this.userRepository.updateVerifyAccount(email);
   }
 
   async updateProfile(id, updateProfileRequest: UpdateProfileRequest): Promise<User | null> {
     return await this.userRepository.updateProfile(id, updateProfileRequest);
+  }
+
+  async updateAvatar(userId: string, avatarUrl: string): Promise<User | null> {
+    return await this.userRepository.updateAvatar(userId, avatarUrl);
   }
 }
